5cf355e3e404fb481348bed081e611973f7a0681
Naser Alkuhili <k4gam3r@gmail.com>
2023-02-01 22:49:18 -0600

    first commit for HW2

--- Week 1/Q3.py
+++ Week 1/Q3.py
@@ -5,7 +5,6 @@ import random
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
 
 
 # Data
